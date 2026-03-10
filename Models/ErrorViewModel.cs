2c618eceaf246711ec7d7711114acb6b2c09a650
diana <diana@5CG9033ZSN>
2026-03-10 17:51:55 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BancaUT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
